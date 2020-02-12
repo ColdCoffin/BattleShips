2971a2244b539be34a86b3d1e869b6ee398e18e6
Roland Rostohar <rorostoh@vsite.hr>
2020-02-12 14:29:00 +0100

    Created player board

--- BattleShips/Form1.cs
+++ BattleShips/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BattleShips
 {
-	public partial class Form1 : Form
+	public partial class Game_Screen : Form
 	{
-		public Form1()
+		public Game_Screen()
 		{
 			InitializeComponent();
 		}
