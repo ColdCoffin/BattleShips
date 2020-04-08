e7b529a6af63261dc2e3733cb883b35cc6d8d940
Roland Rostohar <rorostoh@vsite.hr>
2020-04-08 16:22:33 +0200

    implemented Stats Tracker class

--- BattleShips/StatsTracker.cs
+++ BattleShips/StatsTracker.cs
@@ -8,5 +8,29 @@ namespace BattleShips
 {
 	class StatsTracker
 	{
+
+		public int shipsDestroyed { get; set; }
+		public int shipsHit { get; set; }
+		public int shipsMissed { get; set; }
+		public float accuracy { get { calculateAccuracy(); return accuracy; } set { accuracy = value; } }
+
+
+		public StatsTracker()
+		{
+
+			shipsDestroyed = 0;
+			shipsHit = 0;
+			shipsMissed = 0;
+			accuracy = 0;
+
+		}
+
+		private void calculateAccuracy()
+		{
+			accuracy = (  shipsHit / (shipsHit + shipsMissed)) * 100f;
+		}
+
 	}
+
+	
 }
